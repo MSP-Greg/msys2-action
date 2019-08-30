cacbedb6f5a038bdb9b12afc55b863d9bd23b061
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-08-30 14:07:31 -0500

    Add update_msys2

---
+++ update_msys2/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const tc   = require("@actions/tool-cache");
+
+async function run() {
+  try {
+    if (process.platform === 'win32') {
+      // setup and update MSYS2
+      await exec.exec(`bash.exe -c "pacman-key --init"`);
+      await exec.exec(`bash.exe -c "pacman-key --populate msys2"`);
+      await exec.exec('pacman.exe -Sy --noconfirm --needed --noprogressbar');
+      // await exec.exec('pacman.exe -Syu --noconfirm --needed --noprogressbar');
+      // await exec.exec('pacman.exe -Su  --noconfirm --needed --noprogressbar');
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+run();
