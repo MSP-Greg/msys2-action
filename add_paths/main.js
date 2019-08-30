52de9ce5b1137a4f19cf91ac6828ef2c367229e0
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-08-30 08:14:36 -0500

    again...

--- add_paths/main.js
+++ add_paths/main.js
@@ -5,19 +5,23 @@ const exec   = require('@actions/exec');
 const tc     = require('@actions/tool-cache');
 const c_proc = require('child_process');
 
+
+function cleanPath(path) {
+  const newPath = path.replace(/[^;]+?(Strawberry|CMake|OpenSSL )[^;]*;/g, '');
+  process.env['PATH'] = newPath;
+}
+
 async function run() {
   try {
     if (process.platform === 'win32') {
-      const newPath = process.env['PATH'].replace(/[^;]+?(Strawberry|CMake|OpenSSL )[^;]*;/g, '');
-      console.log(newPath);
-      process.env['PATH'] = newPath;
-      console.log('--------------------------------------');
-      console.log(process.env['PATH']);
-
       const topDir = c_proc.execSync(`ruby.exe -e "STDOUT.write RbConfig::TOPDIR"`).toString().trim();
 
       await core.addPath(`${topDir}/msys64/mingw64/bin`);
       await core.addPath(`${topDir}/msys64/usr/bin`);
+
+      cleanPath(process.env['PATH']);
+      console.log('--------------------------------------');
+      console.log(process.env['PATH']);
     }
   } catch (error) {
     core.setFailed(error.message);
