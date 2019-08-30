bf90a651eb64ed746bda8e89c64344ccb59c00d2
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-08-30 13:48:51 -0500

    add update_openssl

--- add_paths/main.js
+++ add_paths/main.js
@@ -1,20 +1,14 @@
 'use strict';
 
-const core   = require('@actions/core');
-const exec   = require('@actions/exec');
-const tc     = require('@actions/tool-cache');
-const c_proc = require('child_process');
-const env    = process.env;
-
-function cleanPath() {
-  const newPath = process.env['PATH'].replace(/[^;]+?(Strawberry|CMake|OpenSSL)[^;]*;/g, '');
-  env['PATH'] = newPath;
-}
+const core  = require('@actions/core');
+const exec  = require('@actions/exec');
+const tc    = require('@actions/tool-cache');
+const child = require('child_process');
 
 function run() {
   try {
     if (process.platform === 'win32') {
-      const topDir = c_proc.execSync(`ruby.exe -e "STDOUT.write RbConfig::TOPDIR"`).toString().trim();
+      const topDir = child.execSync(`ruby.exe -e "STDOUT.write RbConfig::TOPDIR"`).toString().trim();
       core.addPath(`${topDir.replace(/\//g, "\\")}\\msys64\\mingw64\\bin`);
       core.addPath(`${topDir.replace(/\//g, "\\")}\\msys64\\usr\\bin`);
 
---
+++ update_openssl/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const core  = require('@actions/core');
+const exec  = require('@actions/exec');
+const tc    = require('@actions/tool-cache');
+const child = require('child_process');
+
+function run() {
+  try {
+    if (process.platform === 'win32') {
+      const rubyABIVers = child.execSync(`ruby.exe -e "puts RbConfig::CONFIG['ruby_version']"`).toString().trim();
+
+      if (rubyABIVers >= '2.5') {
+        const openssl = 'mingw-w64-x86_64-openssl';
+        exec.exec(`pacman.exe -S --noconfirm --needed --noprogressbar ${openssl}`);
+      }
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+run();
