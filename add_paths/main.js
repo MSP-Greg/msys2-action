a2d734f38962d72cc0653729aef421bedac03fc3
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-08-30 08:04:44 -0500

    Add CMake, OpenSSL

--- add_paths/main.js
+++ add_paths/main.js
@@ -8,7 +8,7 @@ const c_proc = require('child_process');
 async function run() {
   try {
     if (process.platform === 'win32') {
-      const newPath = process.env['PATH'].replace(/[^;]+?Strawberry[^;]*;/g, '');
+      const newPath = process.env['PATH'].replace(/[^;]+?(Strawberry|CMake|OpenSSL)[^;]*;/g, '');
       console.log(newPath);
       process.env['PATH'] = newPath;
 
