627977971697a86854699e00d58f897011891c4f
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-08-30 10:23:19 -0500

    again

--- add_paths/main.js
+++ add_paths/main.js
@@ -5,6 +5,11 @@ const exec   = require('@actions/exec');
 const tc     = require('@actions/tool-cache');
 const c_proc = require('child_process');
 
+function cleanPath() {
+    const newPath = process.env['PATH'].replace(/[^;]+?(Strawberry|CMake|OpenSSL)[^;]*;/g, '');
+    process.env['PATH'] = newPath;
+}
+
 function run() {
   try {
     if (process.platform === 'win32') {
@@ -13,8 +18,11 @@ function run() {
       core.addPath(`${topDir.replace(/\//g, "\\")}\\msys64\\usr\\bin`);
 
       process.env['MAKE'] = `${topDir}/msys64/usr/bin/make.exe`;
-
-      core.cleanPath();
+      process.env.MAKE    = `${topDir}/msys64/usr/bin/make.exe`;
+      cleanPath();
+      console.log(`MAKE=${process.env['MAKE']}`);
+      console.log(`PATH=${process.env['PATH']}`);
+      
     }
   } catch (error) {
     core.setFailed(error.message);
