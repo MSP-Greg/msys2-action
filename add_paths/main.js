5fa7dc7f650f710ea6ff064941494a547a0c6254
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-08-30 19:19:41 -0500

    OpenSSL for 2.4?

--- add_paths/main.js
+++ add_paths/main.js
@@ -8,14 +8,15 @@ const child = require('child_process');
 function run() {
   try {
     if (process.platform === 'win32') {
-      const topDir = child.execSync(`ruby.exe -e "STDOUT.write RbConfig::TOPDIR"`).toString().trim();
-      core.addPath(`${topDir.replace(/\//g, "\\")}\\msys64\\mingw64\\bin`);
-      core.addPath(`${topDir.replace(/\//g, "\\")}\\msys64\\usr\\bin`);
-
-      core.exportVariable('MAKE', `${topDir}/msys64/usr/bin/make.exe`);
-
       const newPath = process.env['PATH'].replace(/[^;]+?(Strawberry|CMake|mingw64|OpenSSL)[^;]*;/g, '');
       core.exportVariable('PATH', newPath);
+
+      const topDir = child.execSync(`ruby.exe -e "STDOUT.write RbConfig::TOPDIR"`).toString().trim();
+
+      core.addPath(`${topDir.replace(/\//g, "\\")}\\msys64\\usr\\bin`);
+      core.addPath(`${topDir.replace(/\//g, "\\")}\\msys64\\mingw64\\bin`);
+
+      core.exportVariable('MAKE', `${topDir}/msys64/usr/bin/make.exe`);
     }
   } catch (error) {
     core.setFailed(error.message);
--- update_openssl/main.js
+++ update_openssl/main.js
@@ -4,15 +4,21 @@ const core  = require('@actions/core');
 const exec  = require('@actions/exec');
 const tc    = require('@actions/tool-cache');
 const child = require('child_process');
+const fs    = require('fs');
 
-function run() {
+async function run() {
   try {
     if (process.platform === 'win32') {
       const rubyABIVers = child.execSync(`ruby.exe -e "puts RbConfig::CONFIG['ruby_version']"`).toString().trim();
 
       if (rubyABIVers >= '2.5') {
         const openssl = 'mingw-w64-x86_64-openssl';
-        exec.exec(`pacman.exe -S --noconfirm --needed --noprogressbar ${openssl}`);
+        await exec.exec(`pacman.exe -S --noconfirm --needed --noprogressbar ${openssl}`);
+      } else {
+        const openssl_2_4 = 'https://sourceforge.net/projects/msys2/files/REPOS/MINGW/x86_64/mingw-w64-x86_64-openssl-1.0.2.p-1-any.pkg.tar.xz'
+        const openssl_2_4_path = await tc.downloadTool(openssl_2_4);
+        
+        await exec.exec(`pacman.exe -Udd --noconfirm --noprogressbar ${openssl_2_4_path}`);
       }
     }
   } catch (error) {
