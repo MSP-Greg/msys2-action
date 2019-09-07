0251913a3f120b272adfd6507fb4451ef7323798
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-09-07 11:57:05 -0500

    probably not

--- update_msys2/main.js
+++ update_msys2/main.js
@@ -4,17 +4,18 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const tc   = require('@actions/tool-cache');
 
-async function run() {
+function run() {
   try {
     if (process.platform === 'win32') {
       // setup and update MSYS2
-      await exec.exec(`bash.exe -c "pacman-key --init"`);
-      await exec.exec(`bash.exe -c "pacman-key --populate msys2"`);
-      await exec.exec('pacman.exe -Syu');
+      exec.exec(`bash.exe -c "pacman-key --init"`);
+      exec.exec(`bash.exe -c "pacman-key --populate msys2"`);
+      exec.exec('pacman.exe -Syu');
+      exec.exec('pacman.exe -Su');
       const args = '--noconfirm --noprogressbar';
       const pre  = ' mingw-w64-x86_64-';
       const pkgs = ['', 'ragel'].join(pre);
-      await exec.exec(`pacman.exe -S ${args} ${pkgs}`);
+      exec.exec(`pacman.exe -S ${args} ${pkgs}`);
             // full update, takes too long
       // await exec.exec('pacman.exe -Syu --noconfirm --needed --noprogressbar');
       // await exec.exec('pacman.exe -Su  --noconfirm --needed --noprogressbar');
