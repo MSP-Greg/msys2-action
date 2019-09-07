4f367d72d13ebdf5c60e52519c63dbe3c7d06d5f
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-09-07 11:11:51 -0500

    update_msys2 add gcc

--- update_msys2/main.js
+++ update_msys2/main.js
@@ -10,8 +10,9 @@ async function run() {
       // setup and update MSYS2
       await exec.exec(`bash.exe -c "pacman-key --init"`);
       await exec.exec(`bash.exe -c "pacman-key --populate msys2"`);
-      await exec.exec('pacman.exe -Sy --noconfirm --needed --noprogressbar');
-      await exec.exec('pacman.exe -S --noconfirm --needed --noprogressbar mingw-w64-x86_64-ragel');
+      await exec.exec('pacman.exe -Sy --noconfirm --needed --noprogressbar --nodeps mingw-w64-x86_64-gcc');
+      await exec.exec('pacman.exe -S  --noconfirm --needed --noprogressbar mingw-w64-x86_64-ragel');
+      // full update, takes too long
       // await exec.exec('pacman.exe -Syu --noconfirm --needed --noprogressbar');
       // await exec.exec('pacman.exe -Su  --noconfirm --needed --noprogressbar');
     }
