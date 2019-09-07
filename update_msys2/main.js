fb53d834d642bfa140dfef34fe2014235d04466c
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-09-07 12:08:20 -0500

    add needed

--- update_msys2/main.js
+++ update_msys2/main.js
@@ -10,14 +10,14 @@ async function run() {
       // setup and update MSYS2
       await exec.exec(`bash.exe -c "pacman-key --init"`);
       await exec.exec(`bash.exe -c "pacman-key --populate msys2"`);
-      const args = '--noconfirm --noprogressbar';
+      const args = '--noconfirm --noprogressbar --needed';
       const pre  = ' mingw-w64-x86_64-';
       // full update, takes too long
       //await exec.exec(`pacman.exe -Syu ${args}`);
       //await exec.exec(`pacman.exe -Su  ${args}`);
       const pkgs = ['', 'binutils', 'crt', 'headers', 'isl', 'libiconv', 'mpc', 'gcc-libs', 'windows-default-manifest', 'winpthreads', 'zlib', 'gcc', 'ragel'].join(pre);
       await exec.exec(`pacman.exe -Sy ${args}`);
-      await exec.exec(`pacman.exe -S ${args} ${pkgs}`);
+      await exec.exec(`pacman.exe -S  ${args} ${pkgs}`);
     }
   } catch (error) {
     core.setFailed(error.message);
